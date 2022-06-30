cad71146fad1bb955effb476bebe39d6b90aff38
Ayou <31991515+Ayouuuu@users.noreply.github.com>
2022-06-30 16:45:42 +0800

    Update ConfigurationPropertySource.java

--- src/main/java/dev/alangomes/springspigot/ConfigurationPropertySource.java
+++ src/main/java/dev/alangomes/springspigot/ConfigurationPropertySource.java
@@ -2,7 +2,7 @@ package dev.alangomes.springspigot;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.springframework.core.env.PropertySource;
- 
+
 class ConfigurationPropertySource extends PropertySource<FileConfiguration> {
 
     ConfigurationPropertySource(FileConfiguration source) {
